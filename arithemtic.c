21a4bccbf219c5c0f7690bb64ace7aedb6084e49
Purvith S <purviskuruba@gmail.com>
2026-06-04 16:29:46 +0530

    Update arithemtic.c
    Removed unused variable 'res' and updated quotient calculation.

--- arithemtic.c
+++ arithemtic.c
@@ -7,13 +7,12 @@ int main()
     printf("Program: Simple Arithemetic Operatins\n");
     printf("----------------------------------------\n");
 
-    float a, b,res;
+    float a, b;
     printf("Enter two numbers: ");
     scanf("%f %f", &a, &b);
     printf("\nSum = %f\n",a+b);
     printf("Differance = %f\n",a-b);
     printf("Product = %f\n",a*b);
-    res = a/b;
     printf("Quotient = %f\n",res);
     return 0;
 }
